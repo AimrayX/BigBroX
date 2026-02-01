2036b8d33718199206d5e49c805dfe0fb73daa81
T.Hall <timothy.hall@gmx.ch>
2026-02-01 16:42:18 +0100

    implemented move generation of all major pieces

--- attack.cpp
+++ attack.cpp
@@ -89,47 +89,184 @@ void attack::computeKingAttacks() {
     }
 }
 
-uint64_t attack::vertHorMask(uint64_t piecePos) {
-    int rank = piecePos / 8;
-    int file = 7 - (piecePos % 8);
+void attack::initRays() {
+  for(int sq = 0; sq < 64; sq++) {
+    int r = sq / 8;
+    int f = sq % 8;
 
-    uint64_t mask = 0ULL;
-    for (int r = rank + 1; r < 7; r++) mask |= 1ULL << (r * 8 + file);
-    for (int r = rank - 1; r > 0; r--) mask |= 1ULL << (r * 8 + file);
-    for (int f = file + 1; f < 7; f++) mask |= 1ULL << (rank * 8 + f);
-    for (int f = file - 1; f > 0; f--) mask |= 1ULL << (rank * 8 + f);
+    for(int i = 1; i < 8; i++) {
+      if(r + i > 7) break;
 
-    return mask;
+      int target = (r + i) * 8 + f;
+      Rays[0][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r - i < 0) break;
+
+      int target = (r - i) * 8 + f;
+      Rays[1][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(f + i > 7) break;
+
+      int target = r * 8 + (f + i);
+      Rays[2][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r - i < 0) break;
+
+      int target = r * 8 + (f - i);
+      Rays[3][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r + i > 7 || f + i > 7) break;
+
+      int target = (r + i) * 8 + (f + i);
+      Rays[4][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r + i > 7 || f - i < 0) break;
+
+      int target = (r + i) * 8 + (f - i);
+      Rays[5][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r - i < 0 || f + i > 7) break;
+
+      int target = (r - i) * 8 + (f + i);
+      Rays[6][sq] |= (1ULL << target);
+    }
+
+    for(int i = 1; i < 8; i++) {
+      if(r - i < 0 || f - i < 0) break;
+
+      int target = (r - i) * 8 + (f - i);
+      Rays[7][sq] |= (1ULL << target);
+    }
+  }
 }
 
-uint64_t attack::diagonalMask(uint64_t piecePos) {
-    int rank = piecePos / 8;
-    int file = 7 - (piecePos % 8);
+uint64_t attack::getBishopAttacks(int square, uint64_t occupancy) {
+  uint64_t attacks = 0ULL;
 
-    uint64_t mask = 0ULL;
-    for (int r = rank + 1, f = file + 1; r < 7 && f < 7; r++, f++) {
-        mask |= 1ULL << (r * 8 + f);
-    }
-    for (int r = rank + 1, f = file - 1; r < 7 && f > 0; r++, f--) {
-        mask |= 1ULL << (r * 8 + f);
-    }
-    for (int r = rank - 1, f = file + 1; r > 0 && f < 7; r--, f++) {
-        mask |= 1ULL << (r * 8 + f);
-    }
-    for (int r = rank - 1, f = file - 1; r > 0 && f > 0; r--, f--) {
-        mask |= 1ULL << (r * 8 + f);
-    }
+  uint64_t ne_ray = Rays[4][square];
+  uint64_t ne_blockers = ne_ray & occupancy;
 
-    return mask;
+  if(ne_blockers == 0) {
+    attacks |= ne_ray;
+  } else {
+    int blocker = __builtin_ctzll(ne_blockers);
+    attacks |= ne_ray ^ Rays[4][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t nw_ray = Rays[5][square];
+  uint64_t nw_blockers = nw_ray & occupancy;
+
+  if(nw_blockers == 0) {
+    attacks |= nw_ray;
+  } else {
+    int blocker = __builtin_ctzll(nw_blockers);
+    attacks |= nw_ray ^ Rays[5][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t se_ray = Rays[6][square];
+  uint64_t se_blockers = se_ray & occupancy;
+
+  if(se_blockers == 0) {
+    attacks |= se_ray;
+  } else {
+    int blocker = 63 - __builtin_clzll(se_blockers);
+    attacks |= se_ray ^ Rays[6][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t sw_ray = Rays[7][square];
+  uint64_t sw_blockers = sw_ray & occupancy;
+
+  if(sw_blockers == 0) {
+    attacks |= sw_ray;
+  } else {
+    int blocker = 63 - __builtin_clzll(sw_blockers);
+    attacks |= sw_ray ^ Rays[7][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  return attacks;
 }
+
+uint64_t attack::getRookAttacks(int square, uint64_t occupancy) {
+  uint64_t attacks = 0ULL;
+
+  uint64_t n_ray = Rays[0][square];
+  uint64_t n_blockers = n_ray & occupancy;
+
+  if(n_blockers == 0) {
+    attacks |= n_ray;
+  } else {
+    int blocker = __builtin_ctzll(n_blockers);
+    attacks |= n_ray ^ Rays[0][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t e_ray = Rays[2][square];
+  uint64_t e_blockers = e_ray & occupancy;
+
+  if(e_blockers == 0) {
+    attacks |= e_ray;
+  } else {
+    int blocker = __builtin_ctzll(e_blockers);
+    attacks |= e_ray ^ Rays[2][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t s_ray = Rays[1][square];
+  uint64_t s_blockers = s_ray & occupancy;
+
+  if(s_blockers == 0) {
+    attacks |= s_ray;
+  } else {
+    int blocker = 63 - __builtin_clzll(s_blockers);
+    attacks |= s_ray ^ Rays[1][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  uint64_t w_ray = Rays[3][square];
+  uint64_t w_blockers = w_ray & occupancy;
+
+  if(w_blockers == 0) {
+    attacks |= w_ray;
+  } else {
+    int blocker = 63 - __builtin_clzll(w_blockers);
+    attacks |= w_ray ^ Rays[3][blocker];
+    attacks |= (1ULL << blocker);
+  }
+
+  return attacks;
+}
+
+uint64_t attack::getQueenAttacks(int square, uint64_t occupancy) {
+
+  return (attack::getRookAttacks(square, occupancy) | attack::getBishopAttacks(square, occupancy));
+}
+
 
 namespace attack {
     uint64_t knightAttacks[64];
     uint64_t kingAttacks[64];
+    uint64_t Rays[8][64];
 }
 
 void attack::init() {
     computeKnightAttacks();
     computeKingAttacks();
+    initRays();
 }
 
--- attack.hpp
+++ attack.hpp
@@ -5,10 +5,16 @@
 namespace attack {
     void computeKnightAttacks();
     void computeKingAttacks();
+    void initRays();
+    void init();
     extern uint64_t knightAttacks[64];
     extern uint64_t kingAttacks[64];
-    uint64_t vertHorMask(uint64_t piecePos);
-    uint64_t diagonalMask(uint64_t piecePos);
-    void init();
+    extern uint64_t Rays[8][64];
+    uint64_t getRookAttacks(int square, uint64_t occupancy);
+    uint64_t getBishopAttacks(int square, uint64_t occupancy);
+    uint64_t getKingAttacks(int square, uint64_t occupancy);
+    uint64_t getQueenAttacks(int square, uint64_t occupancy);
+    uint64_t getKnightAttacks(int square, uint64_t occupancy);
+    uint64_t getPawnAttacks(int square, uint64_t occupancy);
 }
 
